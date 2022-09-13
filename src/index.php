feedfb2e94e387a38b1c09102a365d73a6af04ed
pawan kumar <98738334+thepawankumar@users.noreply.github.com>
2022-09-13 10:55:21 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello, World from Azure! <br>";
+echo "Hello, World from Azure Cloud! <br>";
 echo "Hello Azure Container Apps! <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
